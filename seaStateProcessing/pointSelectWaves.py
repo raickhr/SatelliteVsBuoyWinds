03db04116f49ba0c41c6f2a71caa377d4a365c2c
Shikhar <ckhr.rai@gmail.com>
2025-07-02 15:11:24 -0400

    update

---
+++ seaStateProcessing/pointSelectWaves.py
@@ -0,0 +1,31 @@
+import xarray as xr
+import numpy as np
+
+LAT_LIST = [-9, -8, -5, -2, 0, 2, 5, 8, 9]
+LON_LIST = [-95, -110, -125, -140, -155, -170, -180, 165]
+
+for lat in LAT_LIST:
+    lat_unit = 'S' if lat < 0 else 'N'
+    for lon in LON_LIST:
+        LON = lon
+        lon_unit = 'W' if lon < 0 else 'E'
+        lon = (lon + 360) % 360
+        folder = '/srv/data2/srai_poseidon/srai_poseidon/observation/SatelliteVsBuoy/downloads/WaveReanalysis/'
+        fileName = f'T_{abs(lat):03.0f}{lat_unit}_{abs(LON):03.0f}{lon_unit}_waveReanalysis.nc'
+        
+        ds = xr.open_dataset(folder + fileName)
+
+        lat_arr = ds['latitude'].to_numpy()
+        lon_arr = (ds['longitude'].to_numpy() + 360)%360
+
+        lon_indx = np.argmin(abs(lon_arr - lon))
+        lat_indx = np.argmin(abs(lat_arr - lat))
+
+        nds = ds.isel(latitude = lat_indx, longitude = lon_indx).drop_vars(['latitude', 'longitude'])
+
+        nds.to_netcdf(folder + f'T_{abs(lat):03.0f}{lat_unit}_{abs(LON):03.0f}{lon_unit}_waveReanalysis_xarr.nc', 
+                      unlimited_dims = 'time')
+
+
+
+
